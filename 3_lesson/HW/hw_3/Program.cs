e1301060f71b850fe2912aa1a9f5f4fee43203f6
Yana <holoimenko.yana@yandex.ru>
2022-12-19 14:36:28 +0700

    HW task3

---
+++ 3_lesson/HW/hw_3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27     5 -> 1, 8, 27, 64, 125
+
+void Chet(int N)
+{
+int i = 0;
+double pere;
+while (i < N )
+{
+i += 1;
+if (i == (N))
+{
+pere = Math.Pow(i,3);
+Console.Write($"{pere}");
+}
+else
+{
+
+pere = Math.Pow(i,3);
+Console.Write($"{pere},");
+}
+}
+
+
+}
+Console.WriteLine("введите число");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
