f568fac19442c338c6edd54fa562a1ec5ea878b9
Yana <holoimenko.yana@yandex.ru>
2022-12-19 13:28:41 +0700

    task3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿void Chet(int N)
+{
+int i = 0;
+double pere;
+while (i < N )
+{
+i += 1;
+if (i == (N))
+{
+pere = Math.Pow(i,2);
+Console.Write($"{pere}");
+}
+else
+{
+
+pere = Math.Pow(i,2);
+Console.Write($"{pere},");
+}
+}
+
+
+}
+Console.WriteLine("введите ч");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
